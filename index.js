5f8986d0bd5ae38e4c5f83ca3204000b90601c40
SADIA <you@example.com>
2026-06-10 12:54:57 +0600

    add delete destination API route

--- index.js
+++ index.js
@@ -64,6 +64,14 @@ async function run() {
       res.send(result);
     })
 
+    app.delete('/destination/:id', async (req, res) =>{
+      const id = req.params.id;
+      const result = await destinationCollection.deleteOne({ _id: new ObjectId(id) },)
+      res.send(result)
+    })
+
+
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
